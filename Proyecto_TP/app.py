cdf5a9337404622bd4d515f2026b6018c2ef7d7d
Martin Silva <56087835+martins80@users.noreply.github.com>
2023-12-02 03:06:05 -0300

    se crea app.py + actuali rsva

---
+++ Proyecto_TP/app.py
@@ -0,0 +1,65 @@
+# Instalar con pip install Flask
+from flask import Flask, request, jsonify, render_template
+#from flask import request
+
+# Instalar con pip install flask-cors
+from flask_cors import CORS
+
+# Instalar con pip install mysql-connector-python
+import mysql.connector
+
+# Si es necesario, pip install Werkzeug
+from werkzeug.utils import secure_filename
+
+# No es necesario instalar, es parte del sistema standard de Python
+import os
+import time
+#--------------------------------------------------------------------
+
+
+
+app = Flask(__name__)
+CORS(app)  # Esto habilitará CORS para todas las rutas
+
+#--------------------------------------------------------------------
+class Catalogo:
+    #----------------------------------------------------------------
+    # Constructor de la clase
+    def __init__(self, host, user, password, database):
+        # Primero, establecemos una conexión sin especificar la base de datos
+        self.conn = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password
+        )
+        self.cursor = self.conn.cursor()
+
+        # Intentamos seleccionar la base de datos
+        try:
+            self.cursor.execute(f"USE {database}")
+        except mysql.connector.Error as err:
+            # Si la base de datos no existe, la creamos
+            if err.errno == mysql.connector.errorcode.ER_BAD_DB_ERROR:
+                self.cursor.execute(f"CREATE DATABASE {database}")
+                self.conn.database = database
+            else:
+                raise err
+
+        # Una vez que la base de datos está establecida, creamos la tabla si no existe
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS productos (
+            codigo INT,
+            Nombre VARCHAR(255) NOT NULL,
+            Apellido VARCHAR(255) NOT NULL,
+            dni INT NOT NULL,
+            FeIng INT,
+            FeEgr INT,
+            Hus INT,               
+            email VARCHAR(255) NOT NULL,
+            imagen_url VARCHAR(255),
+            Mensaje VARCHAR(500) NOT NULL)
+                            ''')
+        self.conn.commit()
+
+        # Cerrar el cursor inicial y abrir uno nuevo con el parámetro dictionary=True
+        self.cursor.close()
+        self.cursor = self.conn.cursor(dictionary=True)
